e1cf65354d5d515b7b1c5c78a3302a134f48a0c1
mukut khandelwal <mukutkhandelwal@gmail.com>
2024-07-14 16:30:52 +0530

    navbar completed

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 
-import {setState, useState} from "react";
+import { useState} from "react";
 import  "./Navbar.css"
 import  Logo  from "../../assets/Greenify.png"
 import { Button } from "@/components/ui/button"
@@ -29,8 +29,9 @@ export default function Navbar() {
             <Link to={"/login"}><CircleUser className="h-4 w-4 stroke-2"/> &nbsp; Login &nbsp; <ArrowDown className="h-3 w-3 transform transition duration-300 ease-in-out group-hover:rotate-180"/></Link>
             
             </Button>
-             {isHovered && <ul className="bg-white h-10  absolute rounded ">
-              <li className="flex gap-x-8 text-sm py-5">
+             {isHovered && <ul className="bg-gray-100 h-10 shadow-2xl absolute rounded " onMouseEnter={()=>setHovered(true)}
+           onMouseLeave={()=>setHovered(false)}>
+              <li className="flex gap-x-8 text-sm py-5 px-5 -translate-y-3">
                 <span>New User?</span>
                 <span className="text-blue-500" ><Link to={'/signup'}>Sign Up</Link></span>
               </li>
