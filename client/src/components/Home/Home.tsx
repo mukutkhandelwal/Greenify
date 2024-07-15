35b3a10981c13ef9a9fe8d55095e067509d6d0f9
mukut khandelwal <mukutkhandelwal@gmail.com>
2024-07-15 23:10:37 +0530

    updating home page

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -5,16 +5,18 @@ export default function Home() {
   return (
     <div className="bg-slate-100 pt-3">
       <div className="bg-white m-4 h-20 py-7  ">
-        <ul className="flex items-center  ">
-          <li className="group">Fashion
+        <ul className="flex items-center justify-around ">
+          <li className="group cursor-pointer">
+            Fashion <ArrowDown className="h-3 w-3 transform transition duration-300 ease-in-out group-hover:rotate-180 ml-16 " style={{"marginTop":"-1.1rem"}}/>
+          
+          </li>   
+
+          <li className="group cursor-pointer">Home&Furniture
+          <ArrowDown className="h-3 w-3 transform transition duration-300 ease-in-out group-hover:rotate-180 ml-16" style={{"marginTop":"-1.1rem","marginLeft":"7.5rem"}}/>
           </li>
-          <ArrowDown className="h-3 w-3 transform transition duration-300 ease-in-out group-hover:rotate-180"/>
-          <li className="group">Electronics
-       
-          </li>   <ArrowDown className="h-3 w-3 transform transition duration-300 ease-in-out group-hover:rotate-180"/>
-          <li className="group">Home&furniture
+          <li className="group cursor-pointer ">Electronics
+          <ArrowDown className="h-3 w-3 transform transition duration-300 ease-in-out group-hover:rotate-180 ml-16"style={{"marginTop":"-1.1rem","marginLeft":"5.3rem"}}/>
           </li>
-          <ArrowDown className="h-3 w-3 transform transition duration-300 ease-in-out group-hover:rotate-180"/>
         </ul>
       </div>
     </div>
